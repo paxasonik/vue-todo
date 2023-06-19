80da4607445c3df4299c8dbf4820eb5dfa595ce6
paxasonik <paxasonk@mail.ru>
2023-06-19 09:46:05 +0300

    added main page, header component and style

---
+++ src/pages/Main.vue
@@ -0,0 +1,29 @@
+<script>
+import {defineComponent} from 'vue'
+import Header from "../components/Header.vue";
+
+
+export default defineComponent({
+  name: "Main",
+  components: {
+    Header
+  }
+})
+</script>
+
+<template>
+  <div class="main__page">
+    <Header/>
+    <section class="content">
+      <div class="container">
+        <h1>
+          Главная страница
+        </h1>
+      </div>
+    </section>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
